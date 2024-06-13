e131b2073faeed176ab748755c74e74b1a913f3b
Rob Martin <rmartin3@alumni.unca.edu>
2024-06-12 22:08:27 -0400

    basic navigation

---
+++ frontend/app/components/navigation.tsx
@@ -0,0 +1,28 @@
+import NavItem from "./nav.item";
+import {
+  // BarChartIcon,
+  // UploadIcon,
+  ListBulletIcon,
+  HomeIcon,
+} from "@radix-ui/react-icons";
+import Logo from "./logo";
+
+export default function Navigation() {
+  return (
+    <div className="h-screen">
+      <div className="flex-grow flex flex-col justify-end items-center">
+        <NavItem to="/" icon={<Logo height={30} width={30} />} title="" />
+        <NavItem
+          to={`/`}
+          icon={<HomeIcon height={28} width={28} />}
+          title="Home"
+        />
+        <NavItem
+          to={`/products`}
+          icon={<ListBulletIcon height={28} width={28} />}
+          title="Datasets"
+        />
+      </div>
+    </div>
+  );
+}
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,12 +1,23 @@
 import type { MetaFunction } from "@remix-run/node";
+import Navigation from "../components/navigation";
+import { Outlet } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Inventory" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex">
+      <div className="w-auto border-x dark:border-neutral-700">
+        <Navigation />
+      </div>
+      <div className="w-full border-r dark:border-neutral-700">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
