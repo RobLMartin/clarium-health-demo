16338a1e725903c0c4fdf1b4c5bfd295b4d74f03
Rob Martin <rmartin3@alumni.unca.edu>
2024-06-12 21:08:52 -0400

    register historical inventory

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from config import app, db
 from routes.product_routes import product_bp
+from routes.historical_inventory_routes import historical_inventory_bp
 
 app.register_blueprint(product_bp)
+app.register_blueprint(historical_inventory_bp)
 
 if __name__ == "__main__":
     with app.app_context():
